acc2e7af2b5c074107e73beb827bb4426bc0b762
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2018-11-15 13:08:43 +0100

    Update nmbs.py

--- sensor/nmbs.py
+++ sensor/nmbs.py
@@ -3,6 +3,7 @@ Shows the available amount of public city bikes for Velo Antwerpen.
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/sensor.nmbs/
 """
+from __future__ import division
 
 import logging
 
@@ -21,12 +22,14 @@ DEFAULT_ICON = 'mdi:train'
 
 CONF_STATION_FROM = 'station_from'
 CONF_STATION_TO = 'station_to'
+CONF_STATION_LIVE = 'station_live'
 
 REQUIREMENTS = ['pyrail==0.0.3']
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_STATION_FROM): cv.string,
     vol.Required(CONF_STATION_TO): cv.string,
+    vol.Required(CONF_STATION_LIVE): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
 })
 
@@ -39,8 +42,62 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     name = config.get(CONF_NAME)
     station_from = config.get(CONF_STATION_FROM)
     station_to = config.get(CONF_STATION_TO)
+    station_live = config.get(CONF_STATION_LIVE)
 
-    add_entities([NMBSSensor(name, station_from, station_to, api_client)], True)
+    add_entities([
+        NMBSSensor(name, station_from, station_to, api_client),
+        NMBSLiveBoard(name, station_live, api_client)
+    ], True)
+
+
+class NMBSLiveBoard(Entity):
+    def __init__(self, name, live_station, api_client):
+        self._name = "NMBS Live"
+        self._station = live_station
+        self._api_client = api_client
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def icon(self):
+        """Return the icon to use in the frontend, if any."""
+        if self._attrs is not None and int(self._attrs["delay"]) > 0:
+            return "mdi:bus-alert"
+        else:
+            return "mdi:bus-clock"
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        return self._state
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes of the sensor."""
+        if self._attrs is not None:
+            return {
+                "Delay": "{} seconds".format(self._attrs["delay"]),
+                "Vehicle ID": self._attrs['vehicle'],
+                "Occupancy": self._attrs['occupancy']['name'],
+                "Extra train": 'Yes' if int(self._attrs['isExtra']) > 0 else 'No',
+                "Departure": "TODO:",
+                ATTR_ATTRIBUTION: "https://api.irail.be/",
+            }
+        else:
+            return {
+                ATTR_ATTRIBUTION: "https://api.irail.be/",
+            }
+
+    def update(self):
+        liveboard = self._api_client.get_liveboard(self._station)
+        next_departure = liveboard["departures"]["departure"][0]
+
+        self._attrs = next_departure
+        self._state = "Track {} - {}".format(next_departure["platform"], next_departure["station"])
 
 
 class NMBSSensor(Entity):
