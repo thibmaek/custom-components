ec538164c6b065cc7a51191b68e5633e43ecd6a0
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-01-07 12:55:15 +0100

    Show station on map

--- sensor/velo_antwerpen/velo_antwerpen.py
+++ sensor/velo_antwerpen/velo_antwerpen.py
@@ -12,7 +12,8 @@ import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (ATTR_ATTRIBUTION, CONF_NAME)
+from homeassistant.const import (
+    ATTR_ATTRIBUTION, CONF_NAME, ATTR_LONGITUDE, ATTR_LATITUDE, CONF_SHOW_ON_MAP)
 from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
@@ -28,6 +29,7 @@ RESOURCE_URL = "https://www.velo-antwerpen.be/availability_map/getJsonObject"
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_STATION_ID): cv.string,
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Optional(CONF_SHOW_ON_MAP, default=False): cv.boolean,
 })
 
 
@@ -35,17 +37,19 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Velo sensor."""
     name = config[CONF_NAME]
     station_id = config.get(CONF_STATION_ID)
+    show_on_map = config.get(CONF_SHOW_ON_MAP)
 
-    add_entities([VeloSensor(name, station_id)], True)
+    add_entities([VeloSensor(name, station_id, show_on_map)], True)
 
 
 class VeloSensor(Entity):
     """Get the available amount of bikes and set the station attributes."""
 
-    def __init__(self, name, station_id):
+    def __init__(self, name, station_id, show_on_map):
         """Initialize the Velo sensor."""
         self._name = name
         self._station_id = station_id
+        self._show_on_map = show_on_map
         self._attrs = {}
         self._station_data = {}
         self._state = None
@@ -71,21 +75,33 @@ class VeloSensor(Entity):
         return self._state
 
     @property
+    def entity_picture(self):
+        """The Velo Antwerpen logo (to display on the map)"""
+        if self._show_on_map:
+            return "https://pbs.twimg.com/profile_images/378800000450946830/8ccde5303e8785905d439c5286c84333_400x400.jpeg"
+
+        return None
+
+    @property
     def device_state_attributes(self):
         """Return the state attributes of the sensor."""
         if self._state is None or self._station_data is None:
             return {}
 
-        return {
+        attrs = {
             'available_slots': int(self._station_data['slots']),
-            'latitude': self._station_data['lat'],
-            'longitude': self._station_data['lon'],
             "station_address": self._station_data["address"],
             "station_id": int(self._station_id),
             "station_name": self._station_data['name'],
             "station_opened": self._station_data['status'] == 'OPN',
             ATTR_ATTRIBUTION: "https://www.velo-antwerpen.be/",
         }
+
+        if self._show_on_map:
+            attrs[ATTR_LATITUDE] = self._station_data['lat']
+            attrs[ATTR_LONGITUDE] = self._station_data['lon']
+
+        return attrs
 
     def make_request(self):
         """Perform the API request to the Velo Antwerpen API"""
